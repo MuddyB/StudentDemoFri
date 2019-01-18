a6ac997baffc8bf4b0c87f7c9489c88dc9a25b2d
MuddyB <baigmou@sheridancollege.ca>
2019-01-18 15:20:43 -0500

    New Filed - Program added

--- src/ca/sheridancollege/week1/softwarefundamentals/Student.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/Student.java
@@ -17,7 +17,10 @@ public class Student {
      */
     private String name;
     private int ID;
-
+    private String Program;
+    
+    
+    
     public Student(String givenName, int givenID)
     {
         name=givenName;
